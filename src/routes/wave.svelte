c971e0b0cf5ff9f38431b46242eeaa3fe738d555
Dick <zoejwickham@gmail.com>
2025-06-19 21:07:53 -0400

    Let wave use any color

--- src/routes/wave.svelte
+++ src/routes/wave.svelte
@@ -1,5 +1,5 @@
 <script>
-  let { size } = $props();
+  let { size, color } = $props();
 </script>
 
 <svg
@@ -10,47 +10,47 @@
   xmlns="http://www.w3.org/2000/svg"
 >
   <g clip-path="url(#clip0_62_1464)">
-    <rect x="6" y="1" width="2" height="2" fill="#D1BEEF" />
-    <rect x="10" width="1" height="1" fill="#D1BEEF" />
-    <rect x="11" y="1" width="1" height="1" fill="#D1BEEF" />
-    <rect x="12" y="2" width="1" height="1" fill="#D1BEEF" />
-    <rect x="11" y="9" width="1" height="1" fill="#D1BEEF" />
-    <rect x="9" y="7" width="2" height="1" fill="#D1BEEF" />
-    <rect x="7" y="9" width="1" height="3" fill="#D1BEEF" />
-    <rect x="6" y="10" width="1" height="5" fill="#D1BEEF" />
-    <rect x="5" y="11" width="1" height="3" fill="#D1BEEF" />
-    <rect x="4" y="12" width="1" height="1" fill="#D1BEEF" />
-    <rect y="12" width="1" height="1" fill="#D1BEEF" />
-    <rect x="1" y="13" width="1" height="1" fill="#D1BEEF" />
-    <rect x="2" y="14" width="1" height="1" fill="#D1BEEF" />
-    <rect x="7" y="2" width="2" height="2" fill="#D1BEEF" />
-    <rect x="8" y="3" width="2" height="2" fill="#D1BEEF" />
-    <rect x="9" y="4" width="2" height="2" fill="#D1BEEF" />
-    <rect x="10" y="5" width="2" height="2" fill="#D1BEEF" />
-    <rect x="11" y="6" width="2" height="3" fill="#D1BEEF" />
-    <rect x="14" y="5" width="2" height="8" fill="#D1BEEF" />
-    <rect x="12" y="10" width="2" height="5" fill="#D1BEEF" />
-    <rect x="7" y="12" width="5" height="4" fill="#D1BEEF" />
-    <rect x="8" y="8" width="3" height="4" fill="#D1BEEF" />
-    <rect x="15" y="4" width="1" height="1" fill="#D1BEEF" />
-    <rect x="13" y="9" width="1" height="1" fill="#D1BEEF" />
-    <rect x="14" y="13" width="1" height="1" fill="#D1BEEF" />
-    <rect x="12" y="15" width="1" height="1" fill="#D1BEEF" />
-    <rect x="3" y="2" width="2" height="2" fill="#D1BEEF" />
-    <rect x="4" y="3" width="2" height="2" fill="#D1BEEF" />
-    <rect x="5" y="4" width="2" height="2" fill="#D1BEEF" />
-    <rect x="6" y="5" width="2" height="2" fill="#D1BEEF" />
-    <rect x="8" y="6" width="1" height="1" fill="#D1BEEF" />
-    <rect x="7" y="7" width="1" height="1" fill="#D1BEEF" />
-    <rect x="6" y="8" width="1" height="1" fill="#D1BEEF" />
-    <rect x="5" y="9" width="1" height="1" fill="#D1BEEF" />
-    <rect x="1" y="4" width="2" height="2" fill="#D1BEEF" />
-    <rect x="2" y="5" width="2" height="2" fill="#D1BEEF" />
-    <rect x="3" y="6" width="2" height="2" fill="#D1BEEF" />
-    <rect x="4" y="7" width="2" height="2" fill="#D1BEEF" />
-    <rect x="1" y="8" width="2" height="2" fill="#D1BEEF" />
-    <rect x="2" y="9" width="2" height="2" fill="#D1BEEF" />
-    <rect x="3" y="10" width="2" height="2" fill="#D1BEEF" />
+    <rect x="6" y="1" width="2" height="2" fill={color} />
+    <rect x="10" width="1" height="1" fill={color} />
+    <rect x="11" y="1" width="1" height="1" fill={color} />
+    <rect x="12" y="2" width="1" height="1" fill={color} />
+    <rect x="11" y="9" width="1" height="1" fill={color} />
+    <rect x="9" y="7" width="2" height="1" fill={color} />
+    <rect x="7" y="9" width="1" height="3" fill={color} />
+    <rect x="6" y="10" width="1" height="5" fill={color} />
+    <rect x="5" y="11" width="1" height="3" fill={color} />
+    <rect x="4" y="12" width="1" height="1" fill={color} />
+    <rect y="12" width="1" height="1" fill={color} />
+    <rect x="1" y="13" width="1" height="1" fill={color} />
+    <rect x="2" y="14" width="1" height="1" fill={color} />
+    <rect x="7" y="2" width="2" height="2" fill={color} />
+    <rect x="8" y="3" width="2" height="2" fill={color} />
+    <rect x="9" y="4" width="2" height="2" fill={color} />
+    <rect x="10" y="5" width="2" height="2" fill={color} />
+    <rect x="11" y="6" width="2" height="3" fill={color} />
+    <rect x="14" y="5" width="2" height="8" fill={color} />
+    <rect x="12" y="10" width="2" height="5" fill={color} />
+    <rect x="7" y="12" width="5" height="4" fill={color} />
+    <rect x="8" y="8" width="3" height="4" fill={color} />
+    <rect x="15" y="4" width="1" height="1" fill={color} />
+    <rect x="13" y="9" width="1" height="1" fill={color} />
+    <rect x="14" y="13" width="1" height="1" fill={color} />
+    <rect x="12" y="15" width="1" height="1" fill={color} />
+    <rect x="3" y="2" width="2" height="2" fill={color} />
+    <rect x="4" y="3" width="2" height="2" fill={color} />
+    <rect x="5" y="4" width="2" height="2" fill={color} />
+    <rect x="6" y="5" width="2" height="2" fill={color} />
+    <rect x="8" y="6" width="1" height="1" fill={color} />
+    <rect x="7" y="7" width="1" height="1" fill={color} />
+    <rect x="6" y="8" width="1" height="1" fill={color} />
+    <rect x="5" y="9" width="1" height="1" fill={color} />
+    <rect x="1" y="4" width="2" height="2" fill={color} />
+    <rect x="2" y="5" width="2" height="2" fill={color} />
+    <rect x="3" y="6" width="2" height="2" fill={color} />
+    <rect x="4" y="7" width="2" height="2" fill={color} />
+    <rect x="1" y="8" width="2" height="2" fill={color} />
+    <rect x="2" y="9" width="2" height="2" fill={color} />
+    <rect x="3" y="10" width="2" height="2" fill={color} />
   </g>
   <defs>
     <clipPath id="clip0_62_1464">
